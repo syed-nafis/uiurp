9c5d99febc718db42cf5756bed5cb4b2fdf11c30
Najmol Hasan <115035081+Najmolh@users.noreply.github.com>
2025-03-10 00:41:32 +0600

    Separated the navbar into a reusable file for better maintainability
    Moved navbar to navbar.php for reusability.
    Included it in pages using include (PHP).
    Reduced duplication and improved consistency.

--- index.php
+++ index.php
@@ -9,39 +9,7 @@
 
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand ps-4 p-2" href="index.html">
-            <img src="resources/UIURP.png" alt="" height="65px" weight="65px">
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li class="nav-item me-5">
-                <a class="nav-link active" aria-current="page" href="index.html">Home</a>
-              </li>
-              <li class="nav-item me-5">
-                <a class="nav-link" href="Research_page.html">Projects</a>
-              </li>
-              <li class="nav-item me-5">
-                <a class="nav-link" href="Faculty_Page.html">Faculty</a>
-              </li>
-              <li class="nav-item me-5">
-                <a class="nav-link" href="#">Forum</a>
-              </li>
-            </ul>
-            <span class="">
-              <div class="container-fluid">
-                <a class="navbar-brand" href="Student_Profile.html">
-                  user_name
-                </a>
-              </div>
-            </span>
-          </div>
-        </div>
-      </nav>
+  <?php include 'navbar.php'; ?>
 
   <div id="search">
     <section class="custom-search">
