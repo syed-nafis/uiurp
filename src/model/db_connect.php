e8400893d95d5c2bf3c35f948fbfc01b3eda229b
syed-nafis <www.syed.nafis@gmail.com>
2025-06-17 04:21:19 +0600

    some bug fixing

--- src/model/db_connect.php
+++ src/model/db_connect.php
@@ -4,6 +4,15 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 // MongoDB connection URI
 $uri = "mongodb+srv://uiurp:uiurp12345@uiurp.fluqo.mongodb.net/uiurp?retryWrites=true&w=majority";
 
+function connectToDatabase() {
+    // MongoDB connection URI
+    $uri = "mongodb+srv://uiurp:uiurp12345@uiurp.fluqo.mongodb.net/uiurp?retryWrites=true&w=majority";
+    
+    // Create a new client and connect to the server
+    $client = new MongoDB\Client($uri);
+    return $client;
+}
+
 try {
     // Create a new client and connect to the server
     $client = new MongoDB\Client($uri);
